205198b2888d3ed29ef9b6d3a968725a2c2a44a7
PedroAldama <54598745+PedroAldama@users.noreply.github.com>
2025-01-27 10:06:53 -0700

    Update CountVowels.java

--- CountVowels.java
+++ CountVowels.java
@@ -1,32 +1,33 @@
 import java.util.HashMap;
 
 public class CountVowels {
-    static final HashMap<String,Integer> vowelMap = new HashMap<>();
-    static{
-        vowelMap.put("a", 1);
-        vowelMap.put("e", 1);
-        vowelMap.put("i", 1);
-        vowelMap.put("o", 1);
-        vowelMap.put("u", 1);
-    }
-    
     public static void main(String[] args) {
-        System.out.println(count("Hello World!"));
+        String word = "Hello World";
+        System.out.println(count(word));
+        System.out.println(countV2(word));
+        System.out.println(countV3(word));
     }
 
     public static String count(String cadena){
-        int vowel, consonants;
-        vowel = consonants = 0;
+        Set<Character> vowels = Set.of('a','e','i','o','u');
+        int vowel;
+        vowel = 0;
         String [] data = cadena.toLowerCase().split("");
         for(String letter: data ){
-            if(vowelMap.containsKey(letter)){
+            if(vowelMap.contains(letter))
                 vowel++;
-                continue;
-            }
-            if(!letter.equals(" ")){
-                consonants++;
-            }
         }
-        return "The number of vowel is: " + vowel + " and consonants: " + consonants;
+        return "The number of vowel is: " + vowel;
+    }
+
+    public static String countV2(String cadena){
+        Set<Character> vowels = Set.of('a','e','i','o','u');
+        long vowel = cadena.toLowerCase().chars().filter(c -> vowels.contains((char) c)).count();
+        return "The number of vowel is: " + vowel;
+    }
+ 
+    public static String countV3(String cadena){
+        long vowel = cadena.toLowerCase().chars().filter(c -> "aeiou".indexOf(c) >= 0).count();
+        return "The number of vowel is: " + vowel;
     }
 }
